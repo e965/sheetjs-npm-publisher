3f931235ad254ccc885894373a56608eee51e377
Dmitry Dyachkov <git@e965.ru>
2023-08-07 16:49:15 +0500

    feat: index.js script

---
+++ README.md
@@ -0,0 +1,44 @@
+# [SheetJS](https://sheetjs.com)
+
+> Since the authors of the original package decided to make a dumb protest that most
+of the community can't access their package without [vulnerabilities](https://github.com/advisories/GHSA-4r6h-8v6p-xvw6),
+I decided to make this project. It automatically takes a fresh version of SheetJS
+from their git repository, and publishes to npm if the version is different.
+The whole process is automated and works through Github Actions.
+https://github.com/e965/sheetjs-npm-publisher
+
+The SheetJS Community Edition offers battle-tested open-source solutions for
+extracting useful data from almost any complex spreadsheet and generating new
+spreadsheets that will work with legacy and modern software alike.
+
+[SheetJS Pro](https://sheetjs.com/pro) offers solutions beyond data processing:
+Edit complex templates with ease; let out your inner Picasso with styling; make
+custom sheets with images/graphs/PivotTables; evaluate formula expressions and
+port calculations to web apps; automate common spreadsheet tasks, and much more!
+
+[![Build Status](https://saucelabs.com/browser-matrix/sheetjs.svg)](https://saucelabs.com/u/sheetjs)
+
+## Documentation
+
+- [API and Usage Documentation](https://docs.sheetjs.com)
+
+- [Downloadable Scripts and Modules](https://cdn.sheetjs.com)
+
+## Related Projects
+
+- <https://oss.sheetjs.com/notes/>: File Format Notes
+
+- [`ssf`](packages/ssf): Format data using ECMA-376 spreadsheet format codes
+
+- [`xlsx-cli`](packages/xlsx-cli/): NodeJS command-line tool for processing files
+
+- [`test_files`](https://github.com/SheetJS/test_files): Sample spreadsheets
+
+- [`cfb`](https://git.sheetjs.com/SheetJS/js-cfb): Container (OLE/ZIP) format library
+
+- [`codepage`](https://git.sheetjs.com/SheetJS/js-codepage): Legacy text encodings
+
+## License
+
+Please consult the attached LICENSE file for details.  All rights not explicitly
+granted by the Apache 2.0 License are reserved by the Original Author.
---
+++ index.js
@@ -0,0 +1,92 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+import ora from 'ora';
+import { simpleGit } from 'simple-git';
+
+const README_FILE = 'README.md';
+const LOCAL_README_PATH = path.join(process.cwd(), README_FILE);
+
+const NPM_PACKAGE_NAME = 'xlsxn';
+const NPM_REGISTRY_URL = 'https://registry.npmjs.org';
+const NPM_PACKAGE_REGISTRY_URL = new URL(NPM_PACKAGE_NAME, NPM_REGISTRY_URL)
+  .href;
+
+const SHEETJS_GIT_REPOSITORY_URL =
+  'https://git.sheetjs.com/sheetjs/sheetjs.git';
+const SHEETJS_PATH = 'sheetjs';
+const SHEETJS_PACKAGE_PATH = path.join(SHEETJS_PATH, 'package.json');
+const SHEETJS_README_PATH = path.join(SHEETJS_PATH, README_FILE);
+
+const git = simpleGit();
+
+let gitPackageVersion = null;
+let npmPackageVersion = null;
+
+const asyncTask = async (title, task) => {
+  const spinner = ora(title).start();
+  const stop = text => spinner.isSpinning && spinner.succeed(text);
+  const warn = text => spinner.isSpinning && spinner.warn(text);
+
+  await task({ log: stop, warn });
+  stop('Success');
+};
+
+await asyncTask('Clone sheetjs repository', async () => {
+  await git.clone(SHEETJS_GIT_REPOSITORY_URL, SHEETJS_PATH);
+});
+
+await asyncTask('Get package version from repository', async ({ log }) => {
+  const gitPackageFileContent = await fs.readFile(SHEETJS_PACKAGE_PATH);
+  const gitPackage = JSON.parse(gitPackageFileContent);
+  gitPackageVersion = gitPackage.version;
+
+  log(`Success, git version = ${gitPackageVersion}`);
+});
+
+await asyncTask(
+  'Get package version from repository',
+  async ({ log, warn }) => {
+    try {
+      const npmRegistryInfoResponse = await fetch(NPM_PACKAGE_REGISTRY_URL, {
+        method: 'GET',
+      });
+      const npmRegistryInfo = await npmRegistryInfoResponse.json();
+      npmPackageVersion = npmRegistryInfo?.['dist-tags']?.latest;
+    } catch (error) {
+      console.error(error);
+    }
+
+    if (!npmPackageVersion) {
+      warn(
+        'Failed to get a version. The package may not have been published yet',
+      );
+    } else {
+      log(`Success, npm version = ${npmPackageVersion}`);
+    }
+  },
+);
+
+await asyncTask('Check versions', async ({ log }) => {
+  if (gitPackageVersion === npmPackageVersion) {
+    log('Versions are the same, no publishing required');
+    process.exit(0);
+  }
+  log('Passed');
+});
+
+await asyncTask('Replacing a README file in project', async () => {
+  await fs.rm(SHEETJS_README_PATH, { force: true, recursive: true });
+  await fs.copyFile(LOCAL_README_PATH, SHEETJS_README_PATH);
+});
+
+await asyncTask('Patching a package.json file in project', async () => {
+  const gitPackageFileContent = await fs.readFile(SHEETJS_PACKAGE_PATH);
+  const gitPackage = JSON.parse(gitPackageFileContent);
+
+  gitPackage.name = NPM_PACKAGE_NAME;
+
+  const newFileContent = JSON.stringify(gitPackage, null, 2);
+  await fs.rm(SHEETJS_PACKAGE_PATH, { force: true, recursive: true });
+  await fs.writeFile(SHEETJS_PACKAGE_PATH, newFileContent, 'utf-8');
+});
