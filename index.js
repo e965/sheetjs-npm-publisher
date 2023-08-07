422747ff6c8809c9543a0bf511e6d72b26826c85
Dmitry Dyachkov <git@e965.ru>
2023-08-07 23:03:50 +0500

    chore: change prettier settings

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const LOCAL_README_PATH = path.join(process.cwd(), README_FILE);
 
 const NPM_PACKAGE_NAME = '@e965/xlsx';
 const NPM_REGISTRY_URL = 'https://registry.npmjs.org';
-const NPM_PACKAGE_REGISTRY_URL = new URL(NPM_PACKAGE_NAME, NPM_REGISTRY_URL)
-  .href;
+const NPM_PACKAGE_REGISTRY_URL = new URL(NPM_PACKAGE_NAME, NPM_REGISTRY_URL).href;
 
-const SHEETJS_GIT_REPOSITORY_URL =
-  'https://git.sheetjs.com/sheetjs/sheetjs.git';
+const SHEETJS_GIT_REPOSITORY_URL = 'https://git.sheetjs.com/sheetjs/sheetjs.git';
 const SHEETJS_PATH = 'sheetjs';
 const SHEETJS_PACKAGE_PATH = path.join(SHEETJS_PATH, 'package.json');
 const SHEETJS_README_PATH = path.join(SHEETJS_PATH, README_FILE);
@@ -25,70 +23,65 @@ let gitPackageVersion = null;
 let npmPackageVersion = null;
 
 const asyncTask = async (title, task) => {
-  const spinner = ora(title).start();
-  const stop = text => spinner.isSpinning && spinner.succeed(text);
-  const warn = text => spinner.isSpinning && spinner.warn(text);
+	const spinner = ora(title).start();
+	const stop = text => spinner.isSpinning && spinner.succeed(text);
+	const warn = text => spinner.isSpinning && spinner.warn(text);
 
-  await task({ log: stop, warn });
-  stop('Success');
+	await task({ log: stop, warn });
+	stop('Success');
 };
 
 await asyncTask('Cloning the sheetjs repository', async () => {
-  await git.clone(SHEETJS_GIT_REPOSITORY_URL, SHEETJS_PATH);
+	await git.clone(SHEETJS_GIT_REPOSITORY_URL, SHEETJS_PATH);
 });
 
 await asyncTask('Getting a package version from the repository', async ({ log }) => {
-  const gitPackageFileContent = await fs.readFile(SHEETJS_PACKAGE_PATH);
-  const gitPackage = JSON.parse(gitPackageFileContent);
-  gitPackageVersion = gitPackage.version;
+	const gitPackageFileContent = await fs.readFile(SHEETJS_PACKAGE_PATH);
+	const gitPackage = JSON.parse(gitPackageFileContent);
+	gitPackageVersion = gitPackage.version;
 
-  log(`Success, git version = ${gitPackageVersion}`);
+	log(`Success, git version = ${gitPackageVersion}`);
 });
 
-await asyncTask(
-  'Getting a package version from the npm registry',
-  async ({ log, warn }) => {
-    try {
-      const npmRegistryInfoResponse = await fetch(NPM_PACKAGE_REGISTRY_URL, {
-        method: 'GET',
-      });
-      const npmRegistryInfo = await npmRegistryInfoResponse.json();
-      npmPackageVersion = npmRegistryInfo?.['dist-tags']?.latest;
-    } catch (error) {
-      console.error(error);
-    }
+await asyncTask('Getting a package version from the npm registry', async ({ log, warn }) => {
+	try {
+		const npmRegistryInfoResponse = await fetch(NPM_PACKAGE_REGISTRY_URL, {
+			method: 'GET',
+		});
+		const npmRegistryInfo = await npmRegistryInfoResponse.json();
+		npmPackageVersion = npmRegistryInfo?.['dist-tags']?.latest;
+	} catch (error) {
+		console.error(error);
+	}
 
-    if (!npmPackageVersion) {
-      warn(
-        'Failed to get a version. The package may not have been published yet',
-      );
-    } else {
-      log(`Success, npm version = ${npmPackageVersion}`);
-    }
-  },
-);
+	if (!npmPackageVersion) {
+		warn('Failed to get a version. The package may not have been published yet');
+	} else {
+		log(`Success, npm version = ${npmPackageVersion}`);
+	}
+});
 
 await asyncTask('Checking versions', async ({ log }) => {
-  if (gitPackageVersion === npmPackageVersion) {
-    log('Versions are the same, no publishing required');
-    process.exit(1);
-  }
-  log('Passed');
+	if (gitPackageVersion === npmPackageVersion) {
+		log('Versions are the same, no publishing required');
+		process.exit(1);
+	}
+	log('Passed');
 });
 
 await asyncTask('Replacing a README file in project', async () => {
-  await fs.rm(SHEETJS_README_PATH, { force: true, recursive: true });
-  await fs.copyFile(LOCAL_README_PATH, SHEETJS_README_PATH);
+	await fs.rm(SHEETJS_README_PATH, { force: true, recursive: true });
+	await fs.copyFile(LOCAL_README_PATH, SHEETJS_README_PATH);
 });
 
 await asyncTask('Patching a package.json file in project', async () => {
-  const gitPackageFileContent = await fs.readFile(SHEETJS_PACKAGE_PATH);
-  const gitPackage = JSON.parse(gitPackageFileContent);
+	const gitPackageFileContent = await fs.readFile(SHEETJS_PACKAGE_PATH);
+	const gitPackage = JSON.parse(gitPackageFileContent);
 
-  gitPackage.name = NPM_PACKAGE_NAME;
-  gitPackage.repository.url = REPOSITORY_URL;
+	gitPackage.name = NPM_PACKAGE_NAME;
+	gitPackage.repository.url = REPOSITORY_URL;
 
-  const newFileContent = JSON.stringify(gitPackage, null, 2);
-  await fs.rm(SHEETJS_PACKAGE_PATH, { force: true, recursive: true });
-  await fs.writeFile(SHEETJS_PACKAGE_PATH, newFileContent, 'utf-8');
+	const newFileContent = JSON.stringify(gitPackage, null, 2);
+	await fs.rm(SHEETJS_PACKAGE_PATH, { force: true, recursive: true });
+	await fs.writeFile(SHEETJS_PACKAGE_PATH, newFileContent, 'utf-8');
 });
